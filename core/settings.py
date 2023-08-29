e4dd0a08cc4b8fb499151deeac78d1c2a93a238e
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-08-29 13:28:59 +0000

    Initial Commit

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "account",
+    "bonus",
+    "delivery",
+    "payment",
+    "product",
+    "vendors",
 ]
 
 MIDDLEWARE = [
