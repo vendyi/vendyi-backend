31afa3abd9a9de5ffb5b12c8985831bd4fbb95eb
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-08-30 19:45:08 +0000

    Vendor app can be modified

--- vendors/models.py
+++ vendors/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Vendor(models.Model):
+    '''Model definition for Vendor.'''
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        '''Meta definition for Vendor.'''
+
+        verbose_name = 'Vendor'
+        verbose_name_plural = 'Vendors'
+
+    def __str__(self):
+        return self.user.username
