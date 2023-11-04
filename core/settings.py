735155ee80828c7f03bf53b18031ebad2ac42e1a
DeAgusco <deagusco@gmail.com>
2023-11-04 12:55:42 +0000

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -129,7 +129,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Assuming you have set these environment variables correctly
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 
