067d302ed15ca0e7bf04fe4dd1cc84b45e2b895b
DeAgusco <deagusco@gmail.com>
2023-11-09 21:11:01 +0000

    changes

--- core/settings.py
+++ core/settings.py
@@ -110,12 +110,8 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
