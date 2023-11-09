2ddd5832ddef7f57cdd8ccf4e486e272061714bc
DeAgusco <deagusco@gmail.com>
2023-11-09 17:00:20 +0000

    Update

--- core/asgi.py
+++ core/asgi.py
@@ -1,15 +1,17 @@
+# asgi.py
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
-from chat import consumers
-from chat.middleware import TokenOrSessionAuthMiddleware
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 # Import get_asgi_application after setting the environment variable
 from django.core.asgi import get_asgi_application
-
 django_asgi_app = get_asgi_application()
+from chat.middleware import TokenOrSessionAuthMiddleware
+from chat import consumers
+
+
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
