8915122e0022812707afe3dcc96e1fd7aa14e388
DeAgusco <deagusco@gmail.com>
2023-11-09 16:54:12 +0000

    Update

--- core/asgi.py
+++ core/asgi.py
@@ -1,15 +1,18 @@
 import os
-from chat.middleware import TokenOrSessionAuthMiddleware
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from chat import consumers
-from django.core.asgi import get_asgi_application
-#from .routing import websocket_urlpatterns  # Import the routing from routing.py
+from chat.middleware import TokenOrSessionAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
+# Import get_asgi_application after setting the environment variable
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': TokenOrSessionAuthMiddleware(
         URLRouter([
             path("ws/chat/<str:room_name>/", consumers.ChatConsumer.as_asgi()),
