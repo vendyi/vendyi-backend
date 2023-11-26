13e19b7f188bb4e4c331d1e4b74d548055a51c42
DeAgusco <deagusco@gmail.com>
2023-11-26 18:49:54 +0000

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -71,13 +71,13 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_superadmin = models.BooleanField(default=False)
     
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name','phone_number']
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name','phone_number']
 
     objects = MyAccountManager()
 
     def __str__(self):
-        return self.email
+        return self.username
 
     def has_perm(self, perm, obj=None):
         return self.is_admin
